d8b37bcc5668f42ae558c949de4c0abc65ce7a8a
vaporvee <yannik@vaporvee.com>
2024-05-07 22:27:07 +0200

    continued building docs style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,49 +1,57 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import rehypeExternalLinks from "rehype-external-links"
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import rehypeExternalLinks from "rehype-external-links";
+import rehypePrettyCode from "rehype-pretty-code";
+import codeTheme from "./src/styles/moonlight-ii.json";
+import lightCodeTheme from "./src/styles/github_light.json";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'vaporvee Docs',
-			customCss: [
-				'./src/styles/custom.scss',
-			  ],
-			social: {
-				github: 'https://github.com/vaporvee',
-				discord: 'https://discord.com/invite/EBdaTefpWy',
-				linkedin: 'https://linkedin.com/in/yannik-ain/',
-			},
-			editLink: {
-				baseUrl: 'https://github.com/vaporvee/docs/edit/main/',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			components: {
-				Head: "./src/components/Head.astro",
-			}
-		}),
-	],
-	markdown: {
-		rehypePlugins: [
-			[
-			rehypeExternalLinks,
-            {
-              target: '_blank',
-            },
+  integrations: [
+    starlight({
+      title: "vaporvee Docs",
+      customCss: ["./src/styles/custom.scss", "./src/styles/global.scss"],
+      social: {
+        github: "https://github.com/vaporvee",
+        discord: "https://discord.com/invite/EBdaTefpWy",
+        linkedin: "https://linkedin.com/in/yannik-ain/",
+      },
+      editLink: {
+        baseUrl: "https://github.com/vaporvee/docs/edit/main/",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
           ],
-		],
-	}
+        },
+        {
+          label: "Discord SDK Godot",
+          autogenerate: { directory: "discord-rpc-godot" },
+        },
+      ],
+      components: {
+        Head: "./src/components/Head.astro",
+      },
+    }),
+  ],
+  markdown: {
+    syntaxHighlight: false,
+    rehypePlugins: [
+      [
+        rehypePrettyCode,
+        {
+          theme: codeTheme,
+        },
+      ],
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+        },
+      ],
+    ],
+  },
 });
