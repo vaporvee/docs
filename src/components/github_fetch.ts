2c7bf265efd50ac4d2d5c77e0ab15ac121dc75da
vaporvee <yannik@vaporvee.com>
2024-05-14 17:10:37 +0200

    finished github card code base

--- src/components/github_fetch.ts
+++ src/components/github_fetch.ts
@@ -1,21 +1,21 @@
+import colors from './colors.json';
+
 interface Repository {
   stargazers_count: number;
   forks_count: number;
+  description: string;
+  language: string;
 }
-
+//TODO: less dynamic typecript more script in astro head
 class GithubCard extends HTMLElement {
   public owner: string;
-  public repo: string; 
-  public starsSpan: HTMLSpanElement;
-  public forksSpan: HTMLSpanElement;
-  
+  public repo: string;
+  private cachedData: Repository | null = null;
 
   constructor() {
     super();
     this.repo = this.getAttribute("repo") as string;
     this.owner = this.getAttribute("owner") as string;
-    this.starsSpan = document.getElementById('gh_stars') as HTMLSpanElement;
-    this.forksSpan = document.getElementById('gh_forks') as HTMLSpanElement;
   }
 
   static async fetchRepository(owner: string, repo: string): Promise<Repository | null> {
@@ -27,18 +27,47 @@ class GithubCard extends HTMLElement {
     return data;
   }
 
+  update_string() {
+    if (this.cachedData) {
+      setString(this, "gh_description", this.cachedData.description);
+      setString(this, "gh_language", this.cachedData.language);
+      setString(this, "gh_stars", this.cachedData.stargazers_count.toString());
+      setString(this, "gh_forks", this.cachedData.forks_count.toString());
+      const element = this.querySelector('#gh_language_color');
+      if (element instanceof HTMLElement) {
+          element.style.backgroundColor = colors[this.cachedData.language].color;
+        }
+    }
+  }
+
   async update(): Promise<void> {
-    const repository = await GithubCard.fetchRepository(this.owner, this.repo);
-    this.starsSpan.textContent = repository.stargazers_count.toString() || '0';
-    this.forksSpan.textContent = repository.forks_count.toString() || '0';
+    const cachedDataKey = `githubCardData_${this.owner}_${this.repo}`;
+    const cachedData = localStorage.getItem(cachedDataKey);
+    if (cachedData) {
+      this.cachedData = JSON.parse(cachedData);
+      this.update_string();
+    }
+    try {
+      this.cachedData = await GithubCard.fetchRepository(this.owner, this.repo);
+      localStorage.setItem(cachedDataKey, JSON.stringify(this.cachedData));
+      this.update_string();
+    } catch (error) {
+      console.error('Failed to fetch repository data:', error);
+    }
   }
 }
 
 customElements.define('gh-card', GithubCard);
 
-(async () => {
-    const card = document.querySelector('gh-card');
+document.querySelectorAll('gh-card').forEach(async (card) => {
     if (card instanceof GithubCard) {
         await card.update();
     }
-})();
+});
+
+function setString(card: GithubCard, elementID: string, value: string | number) {
+  const element = card.querySelector(`#${elementID}`);
+  if (element) {
+    element.textContent = value.toString();
+  }
+}
